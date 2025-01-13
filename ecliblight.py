7ab1a2caeef58148404d4be130d719d9cff813c4
Ilya Drozd <105290125+DrozdIlya@users.noreply.github.com>
2025-01-13 03:01:21 +0100

    Update ecliblight.py

--- ecliblight.py
+++ ecliblight.py
@@ -149,5 +149,5 @@ if __name__ == '__main__':
 
     print('Fluxes computation...')
     df_moments = calculation(df, avg_period, start, stop, output_path)
-    df_moments.to_csv('output.csv')
+    df_moments.to_csv(f'{output_path}/output.csv')
     
